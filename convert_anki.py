652398b3b44b4982d3b59ffc79c0196be887b6fa
Margherita <mtonon.ieu2023@student.ie.edu>
2026-06-29 22:13:13 +0800

    created the function to parse an anki deck

--- convert_anki.py
+++ convert_anki.py
@@ -8,4 +8,14 @@ def parse_anki_file(file):
     """
     This parses an Anki deck txt file and extracts the flashcards.
     """
-    pass
+    cards = []
+    for line in file:
+        line = line.decode("utf-8").strip()
+        if not line.strip() or line.startswith('#'):
+            continue #we skip empty lines and commments
+        parts = line.strip().split("\t")
+        if len(parts) >= 2:
+            front = parts[0].replace('<br>', '\n').replace('&nbsp;', ' ')
+            back = parts[1].replace('<br>', '\n').replace('&nbsp;', ' ')
+            cards.append({'front': front, 'back': back})
+    return cards  
