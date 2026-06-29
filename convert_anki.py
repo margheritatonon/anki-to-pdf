76ab1605012c06f2862ba0013a6999452af68772
Margherita <mtonon.ieu2023@student.ie.edu>
2026-06-29 22:17:29 +0800

    created a function that draws a single card

--- convert_anki.py
+++ convert_anki.py
@@ -19,3 +19,27 @@ def parse_anki_file(file):
             back = parts[1].replace('<br>', '\n').replace('&nbsp;', ' ')
             cards.append({'front': front, 'back': back})
     return cards  
+
+def draw_card(c, x, y, width, height, text):
+    """
+    Draws a single flashcard.
+    """
+
+    c.setStrokeColor(colors.lightgrey)
+    c.setLineWidth(1)
+    c.rect(x, y, width, height, stroke=1, fill=0)
+
+    c.setFillColor(colors.black)
+    c.setFont("Helvetica", 12)
+
+    lines = text.split('\n')
+    line_height = 14
+    total_text_height = len(lines) * line_height
+    start_y = y + (height / 2) + (total_text_height / 2) - line_height
+
+    for i, line in enumerate(lines):
+        current_y = start_y - (i * line_height)
+        if current_y > y + 5:
+            c.drawCentredString(x + (width / 2), current_y, line.strip())
+
+
